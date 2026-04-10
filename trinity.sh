72f51c42bc3fd74ec297b7d2b7204b9af7a3cdc1
Liz Wu <etw33155@university.edu>
2026-04-10 11:19:54 -0400

    starting to draft trinity script

---
+++ trinity.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --time=96:00:00
+#SBATCH --job-name=trinity
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=100G           
+#SBATCH --threads-per-core=1
+#SBATCH --output=trinity.out
+#SBATCH --error=trinity.err
+#SBATCH --mail-user=etw33155@uga.edu
+#SBATCH --mail-type=END,FAIL
+
+# Load module
+module load Trinity/2.15.2-foss-2023a
+
+$TRINITY_DIR/Trinity --seqType fq \
+--left  \
+--right  \
+--SS_lib_type RF \
+--max_memory 2G \
+--CPU 2 \
+--output /work/nclab/lizzy/GENE8940_project/GENE8940_project-1/trinity_output
