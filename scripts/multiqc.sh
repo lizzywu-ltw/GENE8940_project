f7e4b6a14d7000d62fe8476d02b6edc145e10482
Liz Wu <etw33155@university.edu>
2026-04-16 21:51:17 -0400

    script run for multiqc

--- scripts/multiqc.sh
+++ scripts/multiqc.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #SBATCH --partition=batch
-#SBATCH --ntasks=1
 #SBATCH --time=6:00:00
+#SBATCH --job-name=multiqc
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=10G
-#SBATCH --job-name=fastqc
-#SBATCH --output=%x.out
-#SBATCH --error=%x.err
+#SBATCH --output=multiqc.out
+#SBATCH --error=multiqc.err
 #SBATCH --mail-user=etw33155@uga.edu
 #SBATCH --mail-type=END,FAIL
 
 #load modules FastQC and MultiQC before running
-module load FastQC
-module load MultiQC
+module load FastQC/0.12.1-Java-11
+module load MultiQC/1.28-foss-2024a
 
 indir=/work/nclab/lizzy/GENE8940_project/GENE8940_project-1/ribo_output
-outdir=/work/nclab/lizzy/GENE8940_project/GENE8940_project-1/fastQC_output
+outdir=/work/nclab/lizzy/GENE8940_project/GENE8940_project-1/multiQC_output
 
 
 fastqc $indir/*.fq -o $outdir
